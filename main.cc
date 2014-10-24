8ffc814a9267fa31ec618904b15113121c207b6e
jbaker- <jb239812@ohio.edu>
2014-10-24 10:04:54 -0400

    added more output to descend callback

--- main.cc
+++ main.cc
@@ -140,7 +140,7 @@ extern "C" void keyboard(unsigned char key, int x, int y){
         case 'S':
         case 's': ship_1.ascend(-.1); //down
                   std::cout<< "distance from 0,0,0 = " << length(ship_1.get_position()) << std::endl
-                           << "current position = " << ship_1.get_position() << std::endl;
+                           << "current position = " << ship_1.get_position() << std::endl
                            << "speed = " << ship_1.get_speed() << std::endl;
         break;
 
