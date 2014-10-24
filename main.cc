d761b5cc637f7997ba8c1d65c9db2f8820d72a29
jbaker- <jb239812@ohio.edu>
2014-10-23 22:51:21 -0400

    still debugging

--- main.cc
+++ main.cc
@@ -120,7 +120,7 @@ extern "C" void timerFunc(int value){
 
     GLuint time_per_frame = 12;
 
-    glutTimerFunc(time_per_frame, timerFunc(time_per_frame), 0);
+    glutTimerFunc(time_per_frame, timerFunc, 0);
 }
 
 extern "C" void keyboard(unsigned char key, int x, int y){
@@ -177,7 +177,7 @@ int main(int argc, char **argv){
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
-    glutTimerFunc(timerFunc);
+    glutTimerFunc(12, timerFunc, 0);
 
     glutMainLoop();
 
